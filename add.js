8c90bd8a5d3fa3f2650031280931a100953a912d
4thdraw <4thdraw2@gmail.com>
2023-09-12 20:54:33 +0900

    1강 자바스트립트 저녁반

---
+++ add.js
@@ -0,0 +1,9 @@
+let count = 0;
+    count += 1; //1
+    ++count ;  //2
+    --count ; //1
+
+    count-- ; //1
+    count; // 0
+
+    
---
+++ navi.js
@@ -0,0 +1,13 @@
+const navi =[
+           {
+                text :"네이버",
+                link :"http://www.naver.com",
+                etext  : "naver"
+            },
+            {
+                text :"다음",
+                link :"http://www.daum.net",
+                etext : ""
+            }
+            ]
+console.log(navi[0 * 1].text);
